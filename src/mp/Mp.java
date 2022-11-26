fc780f5cc97c75c1816eb47c6ec32320d3a4db94
Usuario <Usuario@Usuario-PC>
2022-11-26 09:36:09 -0500

    version 1 Segundo mensaje

--- src/mp/Mp.java
+++ src/mp/Mp.java
@@ -17,6 +17,7 @@ public class Mp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Bienvenidos");
+        System.out.println("Bienvenidos 2");
     }
     
 }
